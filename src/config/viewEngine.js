a2ed847d1b27370b14a7abb06df753ccfb79dda2
nguyenhuusuot <nguyenhuusuott@gmail.com>
2024-11-05 15:26:29 +0700

    second commit

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,8 +1,12 @@
 import express from 'express';
-
+import path from 'path';
 let configViewEngine = (app) => {
     app.use(express.static(".src/public"));
+    app.set('views', path.join(process.cwd(), 'src', 'views'));
     app.set("view engine", "ejs");
 }
+
+
+
 
 module.exports = configViewEngine;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({extended : true}))
 viewEngine(app)
 initWebRoutes(app)
 
+connectDB();
 
 let port = process.env.PORT || 8005;
 // Port === undefined means == 8005
